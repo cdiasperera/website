2451e7d401df96aa077ff4c853a8d1783392c081
Channa <cdiasperera@gmail.com>
2020-09-07 15:20:10 +0200

    Updates

---
+++ precache-manifest.6b1a82868bb3660e5fa3c8e7d44e5156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd243837a68ddacbed2575f71cc1f286",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "82217368de9853b8ca53",
+    "url": "/website/static/css/main.fda7effb.chunk.css"
+  },
+  {
+    "revision": "b534a719a37d9b6deb3c",
+    "url": "/website/static/js/2.135708a9.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/website/static/js/2.135708a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82217368de9853b8ca53",
+    "url": "/website/static/js/main.fc387ada.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  }
+]);
